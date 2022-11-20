0bff1d47be191dbf3741f88126e9666e8b747aef
Jedding1 <118119228+Jedding1@users.noreply.github.com>
2022-11-20 11:57:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+
+#installs ubuntu software needed for all tools
+sudo apt-get install adb default-jdk git python3.8 openjdk-8-jdk python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf  
+
 #installs python modules
 pip3 install frida-tools
 pip3 install objection
 pip3 install mobsfscan
-
-#installs ubuntu software needed for all tools
-sudo apt-get install adb default-jdk git python3.8 openjdk-8-jdk python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf  
 
 #installs dex2jar and jadx and puts them in the proper place
 https://github.com/MobSF/Mobile-Security-Framework-MobSF.git && cd Mobile-Security-Framework-MobSF
