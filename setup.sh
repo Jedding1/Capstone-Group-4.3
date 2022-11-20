9f95c2ff8c1d701d5c83a2bc17a0387162dbcfdb
Jedding1 <118119228+Jedding1@users.noreply.github.com>
2022-11-20 11:51:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ pip3 install mobsfscan
 sudo apt-get install adb default-jdk git python3.8 openjdk-8-jdk python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf  
 
 #installs dex2jar and jadx and puts them in the proper place
+https://github.com/MobSF/Mobile-Security-Framework-MobSF.git && cd Mobile-Security-Framework-MobSF
+./setup.sh
 git clone https://github.com/pxb1988/dex2jar.git && cd dex2jar
 ./gradlew  distZip
 mv dex-tools/build/distributions/dex-tools-2.2-SNAPSHOT.zip ~/mobsfscan_auto
